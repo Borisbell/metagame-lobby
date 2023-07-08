19596068b4e5f6bc57b620688c2a5e7847f4abd4
Boris Belov <jmorrison@bk.ru>
2023-07-08 15:28:55 +0300

    added crown to leadeerboard

--- src/leaderboard/leaderboard-row/LeaderboardRow.jsx
+++ src/leaderboard/leaderboard-row/LeaderboardRow.jsx
@@ -1,10 +1,12 @@
+import crow_big from '../../assets/crown_big.svg';
+
 export default function LeaderboardRow({id, position, name, level, points}) {
     return (
       <li className='lb_table-row'>
         <h3>{position}</h3>
         <h3>{name}</h3>
         <div className='button-widget'>
-          <img className="widget-icon" src="src/assets/crown_big.svg"/>
+          <img className="widget-icon" src={crow_big}/>
           <div className='progress-bar_wrapper progress-bar_wrapper-lvl'>
             <div className='progress-bar_bg'></div>
             <div className='progress-bar_indicator progress-bar_indicator-pink'></div>
